013cbed74f567acaba745a7918077dad7ffb3899
Antoine Gros <antoine.gros14@gmail.com>
2022-08-19 11:01:11 +0200

    push fin de journée

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,43 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+  def new
+    # donner l'id de la list
+    # faire un bookmark
+    @bookmark = Bookmark.new
+    # donner la liste des films pour la collection
+    @movies = Movie.all
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmarks_params)
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to list_path(@list)
+    else
+      render 'lists/show', status: :unprocessable_entity
+    end
+  end
+  # def create
+  #   @review = Review.new(review_params)
+  #   @review.restaurant = @restaurant
+  #   if @review.save
+  #     redirect_to restaurant_path(@restaurant)
+  #   else
+  #     render 'restaurants/show', status: :unprocessable_entity
+  #   end
+  # end
+
+  def destroy
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(lists_params)
+    @list.save
+    redirect_to list_path(@list.id)
+  end
+
+  private
+
+  def lists_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+end
