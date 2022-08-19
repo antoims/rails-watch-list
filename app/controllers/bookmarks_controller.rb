497f31962dcb2a928fa2a2e5adb8fbe275523ec7
Antoine Gros <antoine.gros14@gmail.com>
2022-08-19 18:44:03 +0200

    second push

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :set_list, only: %i[new create]
+  # before_action :set_bookmark, only: %i[destroy]
   def new
     # donner l'id de la list
     # faire un bookmark
@@ -28,12 +29,20 @@ class BookmarksController < ApplicationController
   # end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(list), status: :see_other
   end
 
   private
 
   def set_list
     @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def bookmarks_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
-    resources :lists, only: %i[index show new create] do
-      resources :bookmarks, only: %i[new create]
-    end
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: %i[destroy]
 end
